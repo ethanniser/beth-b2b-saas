06efaa1d04eb5468d3146a656d32ecd262e43d82
Ethan Niser <whatplan00@gmail.com>
2023-09-14 16:03:43 -0500

    rename to controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,7 @@
 import Elysia from "elysia";
-import { todosService } from "./todos";
+import { todosService as todosController } from "./todos";
 
 export const api = new Elysia({
   name: "@app/api",
   prefix: "/api",
-}).use(todosService);
+}).use(todosController);
